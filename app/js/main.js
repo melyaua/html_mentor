cc81cbd6e427adcd46315039b3256ce2efc232ea
Ihor Melnychenko <igor.melnychenko@tmx.com.ua>
2021-11-19 01:25:12 +0200

    [fix][tabs] Some media fixes

--- app/js/main.js
+++ app/js/main.js
@@ -73,4 +73,24 @@ $(document).ready(function(){
 		// Change color of everything with class .text-color
 		$(".text-color").css("color", newColor);
 	});
+
+
+	const tabLinks = document.querySelectorAll(".tabs a");
+const tabPanels = document.querySelectorAll(".tabs-panel");
+
+for (let el of tabLinks) {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+
+    document.querySelector(".tabs li.active").classList.remove("active");
+    document.querySelector(".tabs-panel.active").classList.remove("active");
+
+    const parentListItem = el.parentElement;
+    parentListItem.classList.add("active");
+    const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+    const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
+    panel[0].classList.add("active");
+    });
+  }
 });
